84fe5fe21cd25e31bd44b8a4de5e1d0cb617c1bb
noanthro <ailurophobisch@gmail.com>
2019-08-20 09:07:54 +0200

    cords updated

--- main.js
+++ main.js
@@ -22,23 +22,24 @@ async function checkTest(x,y,testFile) {
 	return result.percent
 }
 function interact() {
+	// 4 line in Sources Tab
 	let restreamSourceCords = {
-		x: 350,
-		y: 850,
+		x: 360,
+		y: 880,
 	}
 	let interactContextCords = {
 		x: 400,
-		y: 790,
+		y: 820,
 	}
 	let interactWindowCords = {
-		x: 860,
-		y: 525,
+		x: 870,
+		y: 520,
 	}
 	let interactWindowCloseCords = {
 		x: 1267,
 		y: 228,
 	}
-	robot.setMouseDelay(250)
+	robot.setMouseDelay(450)
 	robot.moveMouse(restreamSourceCords.x,restreamSourceCords.y)
 	robot.mouseClick('right')
 	robot.moveMouse(interactContextCords.x,interactContextCords.y)
@@ -49,20 +50,23 @@ function interact() {
 	robot.mouseClick('left')
 }
 async function main () {
-	if (await checkTest(0,0,'obs.png') <= 0) {
-		if (await checkTest(897,356,'arrow.png') <= 0) {
+	if (await checkTest(3,6,'obs.png') <= 0) {
+		if (await checkTest(881,365,'arrow.png') <= 0) {
 			interact()
 		}
 	}
-};main()
-
+};
+function loop () {
+	setInterval(()=>{
+		main()
+	},1000)
+};loop()
 async function off () {
-	let data = robot.screen.capture(0,0,16,16)
+	let data = robot.screen.capture(3,6,10,10)
 	let image = await new Jimp({
 		data: data.image,
 		width: data.width,
 		height: data.height
 	})
-	console.log(image)
-	image.write('./tests/obs.png')	
+	image.write('./new.png')
 }//;off()
